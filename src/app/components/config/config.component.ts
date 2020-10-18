5aed9a76a9e406037dc5e119a41f07b33cc07d9d
ViniMendes-exe <61298724+ViniMendes-exe@users.noreply.github.com>
2020-10-17 22:11:33 -0300

    Notify update
    agora ao selecionar a notificação o sistema retorna o status de ativação

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.css']
 })
-export class ConfigComponent implements OnInit {
+export class ConfigComponent {
+  public overdue = true
+  public notOverdue = true
+  constructor(public toastController: ToastController) {
+  }
 
-  constructor() { }
+  async expenseNotification(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000
+    })
+    toast.present()
+  }
 
-  ngOnInit(): void {
+  async changeOverdueNotification() {
+    const messageOverdue = `Contas vencidas ${this.overdue ? '' : 'não'} serão notificadas.`
+    await this.expenseNotification(messageOverdue)
+  }
+
+  async changeNotOverdueNotification() {
+    const messageOverdue = `Contas proximas a vencer ${this.notOverdue ? '' : 'não'} serão notificadas.`
+    await this.expenseNotification(messageOverdue)
   }
 
 }
