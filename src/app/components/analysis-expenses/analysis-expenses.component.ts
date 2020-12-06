db2302f9e6a15fc119878852ef485d55195f2290
matheusfalves <matheusf.alves1@hotmail.com>
2020-12-06 18:07:41 -0300

    Grafico Anual, do tipo de barros, com valores reais

--- src/app/components/analysis-expenses/analysis-expenses.component.ts
+++ src/app/components/analysis-expenses/analysis-expenses.component.ts
@@ -56,7 +56,7 @@ export class AnalysisExpensesComponent implements OnInit {
     this.createBarChart()
     await this.createLineChart()
     await this.createPieChart()
-    this.createDoughnutChart()
+    await this.createDoughnutChart()
   }
 
   getSumAllExpensesAnalysis(expenses: ExpenseItem[]) {
@@ -69,27 +69,138 @@ export class AnalysisExpensesComponent implements OnInit {
     return 0.0
   }
 
-  createBarChart() {
+  async createBarChart() {
+    const date = new Date()
+    const month1 = date.getMonth()
+    const year1 = date.getFullYear()
+    const date2 = new Date(date.setMonth(month1 + 1))
+    const date3 = new Date(date.setMonth(date.getMonth() + 1))
+    const date4 = new Date(date.setMonth(date.getMonth() + 1))
+    const date5 = new Date(date.setMonth(date.getMonth() + 1))
+    const date6 = new Date(date.setMonth(date.getMonth() + 1))
+    const date7 = new Date(date.setMonth(date.getMonth() + 1))
+    const date8 = new Date(date.setMonth(date.getMonth() + 1))
+    const date9 = new Date(date.setMonth(date.getMonth() + 1))
+    const date10 = new Date(date.setMonth(date.getMonth() + 1))
+    const date11 = new Date(date.setMonth(date.getMonth() + 1))
+    const date12 = new Date(date.setMonth(date.getMonth() + 1))
+
+    const expensesMonthSum1 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: month1,
+        year: year1,
+      }),
+    )
+
+    const expensesMonthSum2 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date2.getMonth(),
+        year: date2.getFullYear(),
+      }),
+    )
+
+    const expensesMonthSum3 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date3.getMonth(),
+        year: date3.getFullYear(),
+      }),
+    )
+
+    const expensesMonthSum4 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date4.getMonth(),
+        year: date4.getFullYear(),
+      }),
+    )
+
+    const expensesMonthSum5 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date5.getMonth(),
+        year: date5.getFullYear(),
+      }),
+    )
+
+    const expensesMonthSum6 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date6.getMonth(),
+        year: date6.getFullYear(),
+      }),
+    )
+    const expensesMonthSum7 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date7.getMonth(),
+        year: date7.getFullYear(),
+      }),
+    )
+
+    const expensesMonthSum8 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date8.getMonth(),
+        year: date8.getFullYear(),
+      }),
+    )
+
+    const expensesMonthSum9 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date9.getMonth(),
+        year: date9.getFullYear(),
+      }),
+    )
+
+    const expensesMonthSum10 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date10.getMonth(),
+        year: date10.getFullYear(),
+      }),
+    )
+
+    const expensesMonthSum11 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date11.getMonth(),
+        year: date11.getFullYear(),
+      }),
+    )
+
+    const expensesMonthSum12 = this.getSumAllExpensesAnalysis(
+      await this.dataExpensesService.getExpensesByMonthAndYear({
+        month: date6.getMonth(),
+        year: date6.getFullYear(),
+      }),
+    )
+
     this.bars = new Chart(this.barChart.nativeElement, {
       type: 'bar',
       data: {
         labels: [
-          'Janeiro',
-          'Fevereiro',
-          'Março',
-          'Abril',
-          'Junho',
-          'Julho',
-          'Agosto',
-          'Setembro',
-          'Outubro',
-          'Novembro',
-          'Dezembro',
+          this.months[month1],
+          this.months[date2.getMonth()],
+          this.months[date3.getMonth()],
+          this.months[date4.getMonth()],
+          this.months[date5.getMonth()],
+          this.months[date6.getMonth()],
+          this.months[date7.getMonth()],
+          this.months[date8.getMonth()],
+          this.months[date9.getMonth()],
+          this.months[date11.getMonth()],
+          this.months[date12.getMonth()],
         ],
         datasets: [
           {
             label: 'Gastos referente ao Ano de 2020',
-            data: [200.5, 200, 90, 500, 700, 90, 1000, 500, 400, 300, 700],
+            data: [
+              expensesMonthSum1,
+              expensesMonthSum2,
+              expensesMonthSum3,
+              expensesMonthSum4,
+              expensesMonthSum5,
+              expensesMonthSum6,
+              expensesMonthSum7,
+              expensesMonthSum8,
+              expensesMonthSum9,
+              expensesMonthSum10,
+              expensesMonthSum11,
+              expensesMonthSum12,
+            ],
             backgroundColor: [
               'rgb(38, 194, 129)',
               'rgb(255, 0, 0)',
@@ -195,50 +306,6 @@ export class AnalysisExpensesComponent implements OnInit {
       },
     })
   }
-
-  /*
-  //Aqui está o grafico de linha, 2 na tela de exibição
-  createLineChart() {
-    this.bars = new Chart(this.lineCanvas.nativeElement, {
-      type: 'line',
-      data: {
-        labels: ['Janeiro', 'Fevereiro', 'Março'],
-        datasets: [
-          {
-            label: 'Gastos referente ao Ano de 2020',
-            data: [500, 1000, 200],
-            backgroundColor: [
-              'rgb(38, 194, 129)',
-              'rgb(255, 0, 0)',
-              'rgb(20, 0, 255)',
-              'rgb(255, 230, 0)',
-              'rgb(0, 255, 10)',
-              'rgb(60, 0, 70)',
-              'rgb(255, 6, 10)',
-              'rgb(20, 42, 255)',
-              'rgb(255, 230, 87)',
-              'rgb(13, 255, 55)',
-              'rgb(60, 25, 70)',
-              'rgb(255, 99, 99)',
-            ],
-            borderColor: 'rgb(38, 194, 129)',
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
-    })
-  }*/
 
   //Aqui está o grafico de pizza, 3 na tela de exibição
   async createPieChart() {
