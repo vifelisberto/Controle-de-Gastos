403f34761f81a2bccc82ef71b47debfb6da7ca3f
vifelisberto <vi.felis@hotmail.com>
2021-05-27 21:41:48 -0300

    feat: novas opcoes de categoria

--- src/app/components/create-expense/create-expense.component.ts
+++ src/app/components/create-expense/create-expense.component.ts
@@ -3,7 +3,7 @@ import { MonthYear } from './../month-year-select/month-year'
 import { DataExpensesService } from 'src/app/services/data-expenses.service'
 import { Component } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
-import { AlertController } from '@ionic/angular'
+import { ActionSheetController, AlertController } from '@ionic/angular'
 import { Router } from '@angular/router'
 import { ToastController } from '@ionic/angular'
 
@@ -17,6 +17,7 @@ export class CreateExpenseComponent {
   public monthYearSelected: MonthYear
 
   public expense: FormGroup
+  public categoryData: { text: string; icon: string }
 
   constructor(
     public alertController: AlertController,
@@ -24,6 +25,7 @@ export class CreateExpenseComponent {
     private router: Router,
     private dataExpensesService: DataExpensesService,
     private toastController: ToastController,
+    public actionSheetController: ActionSheetController,
   ) {
     const dateNow = new Date()
 
@@ -106,5 +108,135 @@ export class CreateExpenseComponent {
       buttons: [{ text: 'Ok' }],
     })
     await inputAlert.present()
+  }
+
+  async presentActionSheet() {
+    const buttons = [
+      {
+        text: 'Moradia',
+        role: '1',
+        icon: 'home-outline',
+        handler: () => {
+          console.log('Delete clicked')
+        },
+      },
+      {
+        text: 'Supermercado',
+        role: '2',
+        icon: 'storefront-outline',
+        handler: () => {
+          console.log('Share clicked')
+        },
+      },
+      {
+        text: 'TV / Internet / Telefone',
+        role: '3',
+        icon: 'caret-forward-circle',
+        handler: () => {
+          console.log('Play clicked')
+        },
+      },
+      {
+        text: 'Transporte',
+        role: '4',
+        icon: 'car-sport-outline',
+        handler: () => {
+          console.log('Favorite clicked')
+        },
+      },
+      {
+        text: 'Lazer',
+        role: '5',
+        icon: 'bicycle-outline',
+        handler: () => {
+          console.log('Favorite clicked')
+        },
+      },
+      {
+        text: 'Saúde e Beleza',
+        role: '6',
+        icon: 'body-outline',
+        handler: () => {
+          console.log('Favorite clicked')
+        },
+      },
+      {
+        text: 'Lanches',
+        role: '7',
+        icon: 'fast-food-outline',
+        handler: () => {
+          console.log('Favorite clicked')
+        },
+      },
+      {
+        text: 'Roupas',
+        role: '8',
+        icon: 'shirt-outline',
+        handler: () => {
+          console.log('Favorite clicked')
+        },
+      },
+      {
+        text: 'Educação',
+        role: '9',
+        icon: 'school-outline',
+        handler: () => {
+          console.log('Favorite clicked')
+        },
+      },
+      {
+        text: 'Bares',
+        role: '10',
+        icon: 'beer-outline',
+        handler: () => {
+          console.log('Favorite clicked')
+        },
+      },
+      {
+        text: 'Jantar',
+        role: '11',
+        icon: 'restaurant-outline',
+        handler: () => {
+          console.log('Favorite clicked')
+        },
+      },
+      {
+        text: 'Outros',
+        role: '12',
+        icon: 'cart-outline',
+        handler: () => {
+          console.log('Favorite clicked')
+        },
+      },
+      {
+        text: 'Fechar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked')
+        },
+      },
+    ]
+
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Categorias',
+      cssClass: 'my-custom-class',
+      animated: true,
+      buttons,
+    })
+    await actionSheet.present()
+
+    const { role } = await actionSheet.onDidDismiss()
+    console.log('Categoria selecionada', role)
+
+    if (role) {
+      this.expense.controls['category'].setValue(role)
+      const categorySelected = buttons[+role - 1]
+
+      this.categoryData = {
+        text: categorySelected.text,
+        icon: categorySelected.icon,
+      }
+    }
   }
 }
--- src/app/components/update-expense/update-expense.component.ts
+++ src/app/components/update-expense/update-expense.component.ts
@@ -3,7 +3,11 @@ import { ExpenseItem } from './../expense-items/expense-item'
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { FormBuilder, Validators } from '@angular/forms'
-import { AlertController, ToastController } from '@ionic/angular'
+import {
+  ActionSheetController,
+  AlertController,
+  ToastController,
+} from '@ionic/angular'
 import { repeat } from 'src/app/services/repeat'
 
 @Component({
@@ -14,6 +18,7 @@ import { repeat } from 'src/app/services/repeat'
 export class UpdateExpenseComponent implements OnInit {
   public itemUpdate: ExpenseItem
   public currentPortion: string
+  public categoryData: { text: string; icon: string }
 
   public expense = this.formBuilder.group({
     id: ['', Validators.required],
@@ -24,12 +29,120 @@ export class UpdateExpenseComponent implements OnInit {
     repeat: ['', Validators.required],
   })
 
+  buttons = [
+    {
+      text: 'Moradia',
+      role: '1',
+      icon: 'home-outline',
+      handler: () => {
+        console.log('Delete clicked')
+      },
+    },
+    {
+      text: 'Supermercado',
+      role: '2',
+      icon: 'storefront-outline',
+      handler: () => {
+        console.log('Share clicked')
+      },
+    },
+    {
+      text: 'TV / Internet / Telefone',
+      role: '3',
+      icon: 'caret-forward-circle',
+      handler: () => {
+        console.log('Play clicked')
+      },
+    },
+    {
+      text: 'Transporte',
+      role: '4',
+      icon: 'car-sport-outline',
+      handler: () => {
+        console.log('Favorite clicked')
+      },
+    },
+    {
+      text: 'Lazer',
+      role: '5',
+      icon: 'bicycle-outline',
+      handler: () => {
+        console.log('Favorite clicked')
+      },
+    },
+    {
+      text: 'Saúde e Beleza',
+      role: '6',
+      icon: 'body-outline',
+      handler: () => {
+        console.log('Favorite clicked')
+      },
+    },
+    {
+      text: 'Lanches',
+      role: '7',
+      icon: 'fast-food-outline',
+      handler: () => {
+        console.log('Favorite clicked')
+      },
+    },
+    {
+      text: 'Roupas',
+      role: '8',
+      icon: 'shirt-outline',
+      handler: () => {
+        console.log('Favorite clicked')
+      },
+    },
+    {
+      text: 'Educação',
+      role: '9',
+      icon: 'school-outline',
+      handler: () => {
+        console.log('Favorite clicked')
+      },
+    },
+    {
+      text: 'Bares',
+      role: '10',
+      icon: 'beer-outline',
+      handler: () => {
+        console.log('Favorite clicked')
+      },
+    },
+    {
+      text: 'Jantar',
+      role: '11',
+      icon: 'restaurant-outline',
+      handler: () => {
+        console.log('Favorite clicked')
+      },
+    },
+    {
+      text: 'Outros',
+      role: '12',
+      icon: 'cart-outline',
+      handler: () => {
+        console.log('Favorite clicked')
+      },
+    },
+    {
+      text: 'Fechar',
+      icon: 'close',
+      role: 'cancel',
+      handler: () => {
+        console.log('Cancel clicked')
+      },
+    },
+  ]
+
   constructor(
     public alertController: AlertController,
     private dataExpensesService: DataExpensesService,
     private formBuilder: FormBuilder,
     private router: Router,
     private toastController: ToastController,
+    public actionSheetController: ActionSheetController,
   ) {}
 
   ngOnInit() {
@@ -52,6 +165,13 @@ export class UpdateExpenseComponent implements OnInit {
         category: this.itemUpdate.category.toString(),
         repeat: this.itemUpdate.repeat.toString(),
       })
+
+      const categorySelected = this.buttons[+this.itemUpdate.category - 1]
+
+      this.categoryData = {
+        text: categorySelected.text,
+        icon: categorySelected.icon,
+      }
     } else this.router.navigate(['/home'])
   }
 
@@ -96,5 +216,28 @@ export class UpdateExpenseComponent implements OnInit {
       buttons: [{ text: 'Ok' }],
     })
     await inputAlert.present()
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Categorias',
+      cssClass: 'my-custom-class',
+      animated: true,
+      buttons: this.buttons,
+    })
+    await actionSheet.present()
+
+    const { role } = await actionSheet.onDidDismiss()
+    console.log('Categoria selecionada', role)
+
+    if (role) {
+      this.expense.controls['category'].setValue(role)
+      const categorySelected = this.buttons[+role - 1]
+
+      this.categoryData = {
+        text: categorySelected.text,
+        icon: categorySelected.icon,
+      }
+    }
   }
 }
