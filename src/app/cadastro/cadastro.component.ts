baaac2b408e58881a25292e0b28f7cdb7b0c91d6
ViniMendes-exe <61298724+ViniMendes-exe@users.noreply.github.com>
2021-05-31 21:29:04 -0300

    tela de registro (#52)

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.scss'],
+})
+export class CadastroComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
