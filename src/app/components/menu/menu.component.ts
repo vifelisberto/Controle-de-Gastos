315c826b155b78a7654a74617160ed87d7586eea
vifelisberto <vi.felis@hotmail.com>
2021-06-01 22:54:06 -0300

    menu com perfil de usuario

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,13 +1,21 @@
+import { AuthenticationService } from './../../shared/authentication-service'
 import { Component } from '@angular/core'
 import { MenuController } from '@ionic/angular'
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css'],
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
-  constructor(private menu: MenuController) {}
+  user: any
+
+  constructor(
+    private menu: MenuController,
+    private authenticationService: AuthenticationService,
+  ) {
+    authenticationService.userData.subscribe(user => (this.user = user))
+  }
 
   closeMenu() {
     this.menu.close()
